f468af35758f708ae64bb986de7f097de61bc302
Brian Scholer <1260690+briantist@users.noreply.github.com>
2021-07-03 21:44:05 -0400

    whitespace fixes

--- ezenv.sh
+++ ezenv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 while IFS= read -r line ; do
-    if [[ "$line" =~ [^[:space:]] ]] ; then
+    if [[ "$line" =~ ^[^[:space:]] ]] ; then
         eval "$line"
         __var=${line%%=*}
         echo "${__var}=${!__var}" >> "${GITHUB_ENV}"
